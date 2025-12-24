b13330c6a52fa73f15f78a920ac52eb7744940df
Tan Zhong Yan <tanzhongyan.passion@gmail.com>
2025-12-25 00:33:51 +0800

    Feat: seed orders data
    - Increased test user holdings
    - Automatically added more sell orders to facilitate trading
    - Made all flask applications production instead of dev

--- api/atomic/user/app.py
+++ api/atomic/user/app.py
@@ -724,4 +724,4 @@ api.add_namespace(address_ns)
 if __name__ == '__main__':
     with app.app_context():
         seed_data()
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
